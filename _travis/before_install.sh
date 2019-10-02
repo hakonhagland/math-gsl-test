d750ee601920ebc77a19371ffe05d8801309388f
Håkon Hægland <hakon.hagland@gmail.com>
2019-10-02 11:42:12 +0200

    DEBUG: Wed 02 Oct 2019 11:42:12 AM CEST

--- _travis/before_install.sh
+++ _travis/before_install.sh
@@ -71,4 +71,5 @@ echo "TRAVIS_BUILD_DIR=$PWD"
 export LD_LIBRARY_PATH=$GSL_INST_DIR/gsl-${GSL_CURRENT}/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
 PATH=$GSL_INST_DIR/gsl-${GSL_CURRENT}/bin:$PATH
 perl Build.PL && ./Build installdeps --cpan_client cpanm
-./Build dist # create a CPAN dist with latest supported GSL release
+ls -l
+#./Build dist # create a CPAN dist with latest supported GSL release
